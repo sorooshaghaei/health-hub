73f6947b18b87821d377e2c034186600d8992a6f
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-07-21 20:40:36 +0330

    creating header

--- health-hub/src/components/Navbar.jsx
+++ health-hub/src/components/Navbar.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "../styles/HeaderStyle.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar navbar-light frosted-glass" style={{ backgroundColor: "#FFD0D0" }}>
+      <div className="container">
+        <div className="row w-100">
+          <div className="col-2">
+            <div className="row">
+              <i className="fa fa-hard-of-hearing p-5">LOGO IS HERE</i>
+            </div>
+            <div className="row">
+              {/* a user icon */}
+              <i className="fa fa-user p-5">
+                We Provide Better Care Experience For Patients And Providers
+              </i>
+            </div>
+          </div>
+          <div className="col-10">
+            {/* routes for sign in and contact us and about and experties */}
+            <Routes>
+              <Route
+                path="/signIn"
+                element={<h1>Sign In</h1>} // replace this with Sign In component
+              />
+              <Route
+                path="/contact"
+                element={<h1>Contact us</h1>} // replace this with Contact component
+              />
+              <Route
+                path="/about"
+                element={<h1>About</h1>} // replace this with About component
+              />
+              <Route
+                path="/experties"
+                element={<h1>Expertise</h1>} // replace this with Expertise component
+              />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
