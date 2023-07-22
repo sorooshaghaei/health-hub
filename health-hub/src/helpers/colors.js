9f55b0ae34a666c62f711b5dab89bf8935d23ffd
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-07-22 19:19:29 +0330

    color palates

---
+++ health-hub/src/helpers/colors.js
@@ -0,0 +1,24 @@
+// Colors for Health Site Theme
+
+// Background Colors
+export const Background = "#F9F9F9"; // Off-White
+
+// Primary Colors
+export const Teal = "#3AA6B9"; // Teal
+
+// Secondary Colors
+export const Salmon = "#FF9EAA"; // Salmon Pink
+export const LightPink = "#FFD0D0"; // Light Pink
+
+// Text Colors
+export const DarkGray = "#333333"; // Dark Gray
+export const SlightlyDarkerGray = "#222222"; // Slightly Darker Gray
+
+// Accent Colors
+export const Blue = "#007BFF"; // Blue
+export const LightGray = "#C4C4C4"; // Light Gray
+export const Green = "#01B574"; // Green
+export const Red = "#E31A1A"; // Red
+
+// Button text Colors
+export const White = "#FFFFFF"; // White
