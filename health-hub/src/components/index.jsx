94e1f4cdbc2d3d520a4959c803fb28682576e202
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-07-23 00:25:58 +0330

    update

--- health-hub/src/components/index.jsx
+++ health-hub/src/components/index.jsx
@@ -5,9 +5,10 @@ export { default as Header } from "./Header";
 export { default as Login } from "./Login";
 export { default as About } from "./About";
 export { default as ContactUs } from "./ContactUs";
-export { default as Search } from "./Search";
+export { default as SearchNav } from "./SearchNav";
 export { default as Footer } from "./Footer";
 
 /* other components*/
 export { default as Navbar } from "./Navbar";
 export { default as Spinner } from "./Spinner";
+
