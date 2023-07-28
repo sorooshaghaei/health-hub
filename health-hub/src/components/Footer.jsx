c0b695c7c1e3eb5e0d3f9d289096dfddf21bf2b5
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-07-28 17:10:06 +0330

    desining footer

--- health-hub/src/components/Footer.jsx
+++ health-hub/src/components/Footer.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
 
+import logo from "../assets/logo.png";
+
+
+
+
 const Footer = () => {
   return (
     <footer style={{ background: 'linear-gradient(#3AA6B9, #ffffff)' }}>
       <div className="container-fluid py-3" >
         <div className="row">
-          <div className="col text-center">
-            <p className="mb-0">Footer Content Goes Here</p>
+          
+
+          <div className="col">
+          <img className="logo col-3 mx-2 align-items-left d-flex" src={logo} alt="Logo" />
+          </div>
+
+          <div className="col" >
+          All rights reserved
+           </div>
+
+          <div className="col" >
+          Contact us : 09000000000
+           </div>
+            
+
+
           </div>
         </div>
-      </div>
+     
     </footer>
   );
 };
--- health-hub/src/components/index.jsx
+++ health-hub/src/components/index.jsx
@@ -1,14 +1,13 @@
 // better way of importing
 
 /* contact components*/
-export { default as Header } from "./Header";
-export { default as SearchNav } from "./SearchNav";
-export { default as Footer } from "./Footer";
-
-
-
-/* other components*/
-export { default as Navbar } from "./Navbar";
-export { default as Spinner } from "./Spinner";
 export { default as Avatar } from "./Avatar";
-
+export { default as Footer } from "./Footer";
+export { default as MyCalendar } from "./MyCalendar";
+export { default as Navbar } from "./Navbar";
+export { default as OveralVisitorsCard } from "./OveralVisitorsCard";
+export { default as SearchNav } from "./SearchNav";
+export { default as SparkLineChart } from "./SparkLineChart";
+export { default as Spinner } from "./Spinner";
+export { default as TotalPatientsCard } from "./TotalPatientsCard";
+export { default as TotalSurgeriesCard } from "./TotalSurgeriesCard";
