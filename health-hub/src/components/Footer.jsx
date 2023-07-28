4b5a476d1779fe99cf35d1b93f5537797af6c09c
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-07-28 17:14:02 +0330

    update

--- health-hub/src/components/Footer.jsx
+++ health-hub/src/components/Footer.jsx
@@ -1,34 +1,29 @@
-import React from 'react';
+import React from "react";
 
 import logo from "../assets/logo.png";
 
-
-
-
 const Footer = () => {
   return (
-    <footer style={{ background: 'linear-gradient(#3AA6B9, #ffffff)' }}>
-      <div className="container-fluid py-3" >
+    <footer style={{ background: "linear-gradient(#3AA6B9, #ffffff)" }}>
+      <div className="container-fluid py-3">
         <div className="row">
-          
-
           <div className="col">
-          <img className="logo col-3 mx-2 align-items-left d-flex" src={logo} alt="Logo" />
+            <img
+              className="logo col-3 mx-2 align-items-left d-flex"
+              src={logo}
+              alt="Logo"
+            />
           </div>
 
-          <div className="col" >
-          All rights reserved
-           </div>
+          <div className="col d-flex justify-content-center align-items-center ">
+            All rights reserved
+          </div>
 
-          <div className="col" >
-          Contact us : 09000000000
-           </div>
-            
-
-
+          <div className="col d-flex justify-content-center align-items-center">
+            Contact us : 09000000000
           </div>
         </div>
-     
+      </div>
     </footer>
   );
 };
