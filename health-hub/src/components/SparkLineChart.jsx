492d00976468c7676d371def17cf0ed5fabec83d
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-07-28 12:02:15 +0330

    sparkline update

--- health-hub/src/components/SparkLineChart.jsx
+++ health-hub/src/components/SparkLineChart.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Sparklines, SparklinesLine } from "react-sparklines";
+import { Teal } from "../helpers/colors";
 
 const SparkLineChart = ({ data, color }) => {
 
     
-
-
   return (
       
-      <div>
-      <Sparklines data={data} limit={20} width={100} height={30} margin={5}>
-        <SparklinesLine color={color} />
+      <div> 
+      <Sparklines data={data} limit={20} width={400} height={210} margin={5}  >
+        <SparklinesLine color={Teal} />
       </Sparklines>
     </div>
   );
