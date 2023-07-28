464ff5cee8dc85ccfca6cc0d665904f12c102ccf
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-07-28 16:39:07 +0330

    basic of footer

--- health-hub/src/components/Footer.jsx
+++ health-hub/src/components/Footer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer style={{ background: 'linear-gradient(#3AA6B9, #ffffff)' }}>
+      <div className="container-fluid py-3" >
+        <div className="row">
+          <div className="col text-center">
+            <p className="mb-0">Footer Content Goes Here</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
