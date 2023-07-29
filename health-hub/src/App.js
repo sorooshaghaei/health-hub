a6505c22434c4c8b398ef9dfdfc61b0c81bb26a0
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-07-29 22:09:19 +0330

    404,not found. (Page of not found needs to be designed)

--- health-hub/src/App.js
+++ health-hub/src/App.js
@@ -12,6 +12,23 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/Patients" element={<Patients />} />
         <Route path="/Appointments" element={<Appointments />} />
+
+        {/* anything else from upper Routes will be rendered here as a 404 or not found! */}
+        <Route
+          path="*"
+          element={
+            <main
+              style={{
+                padding: "1rem",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
       </Routes>
     </div>
   );
