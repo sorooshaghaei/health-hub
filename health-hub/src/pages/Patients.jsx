763114b656e99c1027a69571f10c1f48fba9ec51
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-07-30 00:32:52 +0330

    basic Patient design

--- health-hub/src/pages/Patients.jsx
+++ health-hub/src/pages/Patients.jsx
@@ -1,12 +1,177 @@
-import React from 'react'
-import { Navbar } from '../components'
+import React from "react";
+import { Navbar } from "../components";
 
 const Patients = () => {
   return (
     <div>
-        <Navbar />
-    </div>
-  )
-}
+      <Navbar />
 
-export default Patients
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <div className="container mt-5 mb-5 ">
+        <table class="table table-hover dropshadow">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">NAME</th>
+              <th scope="col">AGE</th>
+              <th scope="col">TIME</th>
+              <th scope="col">CREATED DAY</th>
+              <th scope="col">STATUS</th>
+              <th scope="col">ACTION</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckIndeterminate"
+                  />
+                  <label
+                    className="form-check-label"
+                    for="flexCheckIndeterminate"
+                  ></label>
+                </div>
+              </th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckIndeterminate"
+                  />
+                  <label
+                    className="form-check-label"
+                    for="flexCheckIndeterminate"
+                  ></label>
+                </div>
+              </th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckIndeterminate"
+                  />
+                  <label
+                    className="form-check-label"
+                    for="flexCheckIndeterminate"
+                  ></label>
+                </div>
+              </th>
+              <td colspan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+};
+
+export default Patients;
