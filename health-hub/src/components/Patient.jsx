08b469c2a5e92adeb6c42935d3fc7416da143b05
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-07-31 15:26:00 +0330

    update

--- health-hub/src/components/Patient.jsx
+++ health-hub/src/components/Patient.jsx
@@ -4,32 +4,30 @@ import assignment from "../assets/assignment.svg";
 import date_range from "../assets/date_range.svg";
 import alarm from "../assets/alarm.svg";
 
-const Patient = ({patient}) => {
+const Patient = ({ patient }) => {
   return (
-    <div>
-      <div
-        className="card shadow border-0 m-3"
-        style={{
-          backgroundColor: VeryLightPink,
-          width: "18rem",
-          textAlign: "left",
-        }}
-      >
-        <div className="card-body">
-          <h5 className="card-title m-3">
-            {" "}
-            <i className="fa fa-user-circle"></i> {patient.name}
-          </h5>
-          <div className="card-text m-3 mt-5">
-            <div className="d-block mt-2">
-              <img src={assignment} alt="assignment" /> {patient.sickness}
-            </div>
-            <div className="d-block mt-2">
-              <img src={date_range} alt="date_range" /> {patient.appointmentDate}
-            </div>
-            <div className="d-block mt-2">
-              <img src={alarm} alt="alarm" /> {patient.appointmentTime}
-            </div>
+    <div
+      className="card shadow border-0 m-3"
+      style={{
+        backgroundColor: VeryLightPink,
+        width: "18rem",
+        textAlign: "left",
+      }}
+    >
+      <div className="card-body">
+        <h5 className="card-title m-3">
+          {" "}
+          <i className="fa fa-user-circle"></i> {patient.name}
+        </h5>
+        <div className="card-text m-3 mt-5">
+          <div className="d-block mt-2">
+            <img src={assignment} alt="assignment" /> {patient.sickness}
+          </div>
+          <div className="d-block mt-2">
+            <img src={date_range} alt="date_range" /> {patient.appointmentDate}
+          </div>
+          <div className="d-block mt-2">
+            <img src={alarm} alt="alarm" /> {patient.appointmentTime}
           </div>
         </div>
       </div>
--- health-hub/src/pages/Patients.jsx
+++ health-hub/src/pages/Patients.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Navbar } from "../components";
-
 import Spinner from "../components/Spinner";
 import Patient from "../components/Patient";
-
 import { LightTeal } from "../helpers/colors";
 
 const Patients = ({ patient, loading }) => {
@@ -17,7 +15,6 @@ const Patients = ({ patient, loading }) => {
           className="btn text-white"
           style={{ backgroundColor: LightTeal }}
         >
-          {" "}
           <i className="fa fa-plus"></i> Add Patient
         </button>
       </div>
@@ -25,9 +22,13 @@ const Patients = ({ patient, loading }) => {
       {loading ? (
         <Spinner />
       ) : (
-        <div className="row mx-3 mt-3">
+        <div className="row mx-4">
           {patient.length > 0 ? (
-            patient.map((p) => <Patient key={p.id} patient={p} />)
+            patient.map((p) => (
+              <div className="col d-flex justify-content-center align-items-center">
+                <Patient patient={p} key={p.id} />
+              </div>
+            ))
           ) : (
             <div className="text-center mt-5">
               <p className="h3">not found!</p>
@@ -36,81 +37,6 @@ const Patients = ({ patient, loading }) => {
           )}
         </div>
       )}
-
-      {/* <div className="container mt-5 mb-5 ">
-        <table class="table table-hover dropshadow">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">NAME</th>
-              <th scope="col">AGE</th>
-              <th scope="col">TIME</th>
-              <th scope="col">CREATED DAY</th>
-              <th scope="col">STATUS</th>
-              <th scope="col">ACTION</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">
-                <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    value=""
-                    id="flexCheckIndeterminate"
-                  />
-                  <label
-                    className="form-check-label"
-                    for="flexCheckIndeterminate"
-                  ></label>
-                </div>
-              </th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">
-                <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    value=""
-                    id="flexCheckIndeterminate"
-                  />
-                  <label
-                    className="form-check-label"
-                    for="flexCheckIndeterminate"
-                  ></label>
-                </div>
-              </th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">
-                <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    value=""
-                    id="flexCheckIndeterminate"
-                  />
-                  <label
-                    className="form-check-label"
-                    for="flexCheckIndeterminate"
-                  ></label>
-                </div>
-              </th>
-              <td colspan="2">Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
- */}
     </div>
   );
 };
