a78077cf8b7313f2e6948e22ef039161397e5aa2
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-09-03 15:36:52 +0330

    viewpatient basic implementing

--- health-hub/src/components/patients/ViewPatient.jsx
+++ health-hub/src/components/patients/ViewPatient.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { LightRed, Teal, VeryLightPink } from "../../helpers/colors";
+import Spinner from "../Spinner";
 
-const ViewPatient = () => {
+const ViewPatient = ({ loading }) => {
   return (
-    <div>ViewPatient</div>
-  )
-}
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <section className="m-5">
+            <div
+              className="card border-0 m-3 col-6"
+              style={{
+                backgroundColor: VeryLightPink,
+                textAlign: "left",
+              }}
+            >
+              <div>
+                <form className="p-5">
+                  <div className="form-group">Full Name: </div>
+                  <div className="form-group">Type Of Sickness: </div>
+                  <div className="form-group">Date: </div>
+                  <div className="form-group">Time: </div>
+                  <div className="form-group">Urgency: </div>
+                  <div className="form-group">Phone Number: </div>
 
-export default ViewPatient
+                  <button
+                    type="submit"
+                    className="btn mt-3 text-white"
+                    style={{ backgroundColor: Teal }}
+                  >
+                    save changes
+                  </button>
+                  <button
+                    to={"/Patients"}
+                    type="delete"
+                    className="btn mt-3 mx-2 text-white"
+                    style={{ backgroundColor: LightRed }}
+                  >
+                    delete patient
+                  </button>
+                </form>
+              </div>
+            </div>
+          </section>
+        </>
+      )}
+    </>
+  );
+};
+export default ViewPatient;
