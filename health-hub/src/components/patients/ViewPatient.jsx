9db4a61082a36a45f0ed1016ac0dcb7f3d76b6da
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-09-05 13:45:40 +0330

    uodate viewPatient design

--- health-hub/src/components/patients/ViewPatient.jsx
+++ health-hub/src/components/patients/ViewPatient.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { LightRed, Teal, VeryLightPink } from "../../helpers/colors";
+import { LightRed, Teal, VeryLightPink} from "../../helpers/colors";
 import Spinner from "../Spinner";
 import { useParams } from "react-router-dom";
 import { getPatientInfo } from "../../services/patientService";
@@ -52,41 +52,41 @@ const ViewPatient = () => {
                   >
                     <div>
                       <div className="p-5">
-                        <h5 name="name" className="form-group mb-3">
-                          Full Name: {patient.name}
+                        <h5 name="name" className="form-group mb-4">
+                          Full Name: <b>{patient.name}</b>
                         </h5>
 
-                        <h5 name="typeOfSickness" className="form-group mb-3">
-                          Type Of Sickness: {patient.typeOfSickness}
+                        <h5 name="typeOfSickness" className="form-group mb-4">
+                          Type Of Sickness: <b>{patient.typeOfSickness}</b>
                         </h5>
-                        <h5 name="appointmentDate" className="form-group mb-3">
-                          Date: {patient.appointmentDate}
+                        <h5 name="appointmentDate" className="form-group mb-4">
+                          Date: <b>{patient.appointmentDate}</b>
                         </h5>
-                        <h5 name="appointmentTime" className="form-group mb-3">
-                          Time: {patient.appointmentTime}
+                        <h5 name="appointmentTime" className="form-group mb-4">
+                          Time: <b>{patient.appointmentTime}</b>
                         </h5>
-                        <h5 name="levelOfUrgency" className="form-group mb-3">
-                          Urgency: {patient.levelOfUrgency}
+                        <h5 name="levelOfUrgency" className="form-group mb-4">
+                          Urgency: <b>{patient.levelOfUrgency}</b>
                         </h5>
-                        <h5 name="phone" className="form-group mb-3">
-                          Phone Number: {patient.phone}
+                        <h5 name="phone" className="form-group mb-4">
+                          Phone Number: <b>{patient.phone}</b>
                         </h5>
-                     
-                      <button
-                        type="submit"
-                        className="btn mt-3 text-white"
-                        style={{ backgroundColor: Teal }}
-                      >
-                        save changes
-                      </button>
-                      <button
-                        to={"/Patients"}
-                        type="delete"
-                        className="btn mt-3 mx-3 text-white"
-                        style={{ backgroundColor: LightRed }}
-                      >
-                        delete patient
-                      </button>
+
+                        <button
+                          type="submit"
+                          className="btn mt-3 text-white"
+                          style={{ backgroundColor: Teal }}
+                        >
+                          save changes
+                        </button>
+                        <button
+                          to={"/Patients"}
+                          type="delete"
+                          className="btn mt-3 mx-3 text-white"
+                          style={{ backgroundColor: LightRed }}
+                        >
+                          delete patient
+                        </button>
                       </div>
                     </div>
                   </div>
