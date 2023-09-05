2959516381231c4c434beb95dce256cd1f82962b
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-09-05 13:28:14 +0330

    viewPatient done

--- health-hub/src/components/patients/ViewPatient.jsx
+++ health-hub/src/components/patients/ViewPatient.jsx
@@ -1,97 +1,103 @@
-// import React, { useEffect, useState } from "react";
-// import { LightRed, Teal, VeryLightPink } from "../../helpers/colors";
-// import Spinner from "../Spinner";
-// import { useParams } from "react-router-dom";
-// import { getPatientInfo } from "../../services/patientService";
+import React, { useEffect, useState } from "react";
+import { LightRed, Teal, VeryLightPink } from "../../helpers/colors";
+import Spinner from "../Spinner";
+import { useParams } from "react-router-dom";
+import { getPatientInfo } from "../../services/patientService";
 
 const ViewPatient = () => {
-//   const { patientID } = useParams();
+  const { patientID } = useParams();
 
-//   const [state, setState] = useState({
-//     loading: false,
-//     patient: {},
-//   });
+  const [state, setState] = useState({
+    loading: false,
+    patient: {},
+  });
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         setState({ ...state, loading: true });
-//         const { date: patientData } = await getPatientInfo(patientID);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setState((prevState) => ({ ...prevState, loading: true }));
+        const { data: patientData } = await getPatientInfo(patientID);
 
-//         setState({ ...state, loading: false, patient: patientData });
-//       } catch (error) {
-//         console.log(error);
-//         setState({ ...state, loading: false });
-//       }
-//     };
-//     fetchData();
-//   }, [patientID]);
+        setState((prevState) => ({
+          ...prevState,
+          loading: false,
+          patient: patientData,
+        }));
+      } catch (error) {
+        console.log(error);
+        setState((prevState) => ({ ...prevState, loading: false }));
+      }
+    };
+    fetchData();
+  }, [patientID]);
 
-//   const { loading, patient } = state;
-//   return (
-//     <>
-//       {loading ? (
-//         <Spinner />
-//       ) : (
-//         // Object.keys(patient).length > 0 &&
-//          (
-//           <>
-//             {
-//               <>
-//                 <section className="m-5">
-//                   <div
-//                     className="card border-0 m-3 col-6"
-//                     style={{
-//                       backgroundColor: VeryLightPink,
-//                       textAlign: "left",
-//                     }}
-//                   >
-//                     <div>
-//                       <form className="p-5">
-//                         <div className="form-group mb-3">
-//                           Full Name: {patient.name}
-//                         </div>
-//                         <div className="form-group mb-3">
-//                           Type Of Sickness: {patient.typeOfSickness}
-//                         </div>
-//                         <div className="form-group mb-3">
-//                           Date: {patient.appointmentDate}
-//                         </div>
-//                         <div className="form-group mb-3">
-//                           Time: {patient.appointmentTime}
-//                         </div>
-//                         <div className="form-group mb-3">
-//                           Urgency: {patient.levelOfUrgency}
-//                         </div>
-//                         <div className="form-group mb-3">
-//                           Phone Number: {patient.phone}
-//                         </div>
+  const { loading, patient } = state;
 
-//                         <button
-//                           type="submit"
-//                           className="btn mt-3 text-white"
-//                           style={{ backgroundColor: Teal }}
-//                         >
-//                           save changes
-//                         </button>
-//                         <button
-//                           to={"/Patients"}
-//                           type="delete"
-//                           className="btn mt-3 mx-2 text-white"
-//                           style={{ backgroundColor: LightRed }}
-//                         >
-//                           delete patient
-//                         </button>
-//                       </form>
-//                     </div>
-//                   </div>
-//                 </section>
-//               </>
-//             }
-//           </>
-//         )
-//       )}
-//     </>
-//   );
- };
+  return (
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        Object.keys(patient).length > 0 && (
+          <>
+            {
+              <>
+                <div className="m-5">
+                  <div
+                    className="card border-0 m-3 col-6"
+                    style={{
+                      backgroundColor: VeryLightPink,
+                      textAlign: "left",
+                    }}
+                  >
+                    <div>
+                      <div className="p-5">
+                        <h5 name="name" className="form-group mb-3">
+                          Full Name: {patient.name}
+                        </h5>
+
+                        <h5 name="typeOfSickness" className="form-group mb-3">
+                          Type Of Sickness: {patient.typeOfSickness}
+                        </h5>
+                        <h5 name="appointmentDate" className="form-group mb-3">
+                          Date: {patient.appointmentDate}
+                        </h5>
+                        <h5 name="appointmentTime" className="form-group mb-3">
+                          Time: {patient.appointmentTime}
+                        </h5>
+                        <h5 name="levelOfUrgency" className="form-group mb-3">
+                          Urgency: {patient.levelOfUrgency}
+                        </h5>
+                        <h5 name="phone" className="form-group mb-3">
+                          Phone Number: {patient.phone}
+                        </h5>
+                     
+                      <button
+                        type="submit"
+                        className="btn mt-3 text-white"
+                        style={{ backgroundColor: Teal }}
+                      >
+                        save changes
+                      </button>
+                      <button
+                        to={"/Patients"}
+                        type="delete"
+                        className="btn mt-3 mx-3 text-white"
+                        style={{ backgroundColor: LightRed }}
+                      >
+                        delete patient
+                      </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            }
+          </>
+        )
+      )}
+    </>
+  );
+};
+
 export default ViewPatient;
