000eb919edb8c0009d2f9f0c46e70eb2eb971a20
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-09-21 01:13:49 +0330

    trying to implement a search patient. it does not work

--- health-hub/src/components/header/SearchNav.jsx
+++ health-hub/src/components/header/SearchNav.jsx
@@ -1,15 +1,34 @@
+import React, { useState } from "react";
 import { Teal } from "../../helpers/colors";
 
 const SearchNav = () => {
+  const [query, setQuery] = useState("");
+  const [patients, setPatients] = useState([]);
+
+  const handleInputChange = (event) => {
+    const searchText = event.target.value.toLowerCase();
+    const searchedPatients = patients.filter((patient) =>
+      patient.name.toLowerCase().includes(searchText)
+    );
+    setQuery(event.target.value);
+    setPatients(searchedPatients);
+  };
+
   return (
     <div>
       <form className="d-flex">
         <input
+          value={query}
+          onChange={handleInputChange}
           className="form-control me-2"
           type="search"
           placeholder="Search patients..."
           aria-label="Search"
         />
+
+        {patients.map((patient) => (
+          <div key={patient.id}>{patient.name}</div>
+        ))}
         <button
           className="btn text-white"
           style={{ backgroundColor: Teal }}
