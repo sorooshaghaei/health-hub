f7e98fd3f978c85c09f44398ce6b37267668410a
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-09-21 12:27:18 +0330

    trying to fix search. not sucessfull

--- health-hub/src/components/header/Navbar.jsx
+++ health-hub/src/components/header/Navbar.jsx
@@ -6,7 +6,7 @@ import { LightPink } from "../../helpers/colors";
 import SearchNav from "./SearchNav";
 import Avatar from "./Avatar";
 
-const Navbar = () => {
+const Navbar = ({patient}) => {
   return (
     <nav
       className="navbar frosted-glass"
@@ -18,7 +18,7 @@ const Navbar = () => {
         </div>
 
         <div className="col-md-4 col-5">
-          <SearchNav />
+          <SearchNav patient={patient}/>
         </div>
 
         <div className="mx-2">
--- health-hub/src/components/header/SearchNav.jsx
+++ health-hub/src/components/header/SearchNav.jsx
@@ -1,41 +1,82 @@
 import React, { useState } from "react";
 import { Teal } from "../../helpers/colors";
+import { Link } from "react-router-dom";
 
-const SearchNav = () => {
-  const [query, setQuery] = useState("");
+const SearchNav = ({ patient }) => {
+  const [query, setQuery] = useState({ text: "" });
   const [patients, setPatients] = useState([]);
+  const [showDropdown, setShowDropdown] = useState(false);
 
+  // const handleInputChange = (event) => {
+  //   setQuery({ ...query, text: event.target.value });
+  //   const allPatients = patient.filter((p) => {
+  //     return p.name.toLowerCase().includes(event.target.value.toLowerCase());
+  //   });
+
+  //   setPatients(allPatients);
+  //   setShowDropdown(true); // Show the dropdown when there are search results.
+  // };
   const handleInputChange = (event) => {
-    const searchText = event.target.value.toLowerCase();
-    const searchedPatients = patients.filter((patient) =>
-      patient.name.toLowerCase().includes(searchText)
-    );
-    setQuery(event.target.value);
-    setPatients(searchedPatients);
+    setQuery({ ...query, text: event.target.value });
+    const allPatients = patient.filter((p) => {
+      return p.name.toLowerCase().includes(event.target.value.toLowerCase());
+    });
+  
+    setPatients(allPatients);
+  
+    // Add the conditional check here to hide the dropdown when input is null.
+    if (event.target.value === null) {
+      setShowDropdown(false);
+    } else {
+      setShowDropdown(true); // Show the dropdown when there are search results.
+    }
+  };
+
+  const handlePatientClick = (selectedPatient) => {
+    setQuery({ text: selectedPatient.name });
+    setShowDropdown(false); // Hide the dropdown when a patient is selected.
+    
   };
 
   return (
     <div>
-      <form className="d-flex">
-        <input
-          value={query}
-          onChange={handleInputChange}
-          className="form-control me-2"
-          type="search"
-          placeholder="Search patients..."
-          aria-label="Search"
-        />
+      <form className="row">
+        <div className="col-9">
+          <input
+            value={query.text}
+            onChange={handleInputChange}
+            className="form-control"
+            type="search"
+            placeholder="Search patients..."
+            aria-label="Search"
+          />
 
-        {patients.map((patient) => (
-          <div key={patient.id}>{patient.name}</div>
-        ))}
-        <button
-          className="btn text-white"
-          style={{ backgroundColor: Teal }}
-          type="submit"
-        >
-          Search
-        </button>
+          {showDropdown && patients.length > 0 && (
+            <div className="dropdown">
+              <ul className="dropdown-menu" style={{ display: "block" }}>
+                {patients.map((patient) => (
+                  <li
+                    key={patient.id}
+                    className="dropdown-item "
+                    onClick={() => handlePatientClick(patient)}
+                  >
+                    {patient.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+        <div className="col-3">
+          <Link
+            to={`/Patients/${patient.id}`}
+            className="btn text-white"
+            style={{ backgroundColor: Teal }}
+            type="submit"
+          >
+            Search
+          </Link>
+        </div>
       </form>
     </div>
   );
