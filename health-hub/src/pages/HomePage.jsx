fe48e917e7c9dd814222f986f4a4c6e2209cb065
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-10-22 09:52:28 +0330

    putting an image for mainpage

--- health-hub/src/pages/HomePage.jsx
+++ health-hub/src/pages/HomePage.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { LightTeal, Salmon} from "../helpers/colors";
+import { LightTeal, Salmon } from "../helpers/colors";
 import { Link } from "react-router-dom";
+import mainpage from "../assets/mainpage.jpeg";
 
 const HomePage = () => {
   return (
     <div>
-      <Link to={"./Register"} className="btn rounded m-5 text-white" style={{ backgroundColor: LightTeal }}>
+      <div className="row col">
+        <img src={mainpage} alt="image" />
+      </div>
+      <Link
+        to={"./Register"}
+        className="btn rounded m-5 text-white"
+        style={{ backgroundColor: LightTeal }}
+      >
         <i className="fa fa-user-plus"></i> register
       </Link>
-      <Link to={"./Login"} className="btn rounded m-5 text-white" style={{ backgroundColor: Salmon }}>
+      <Link
+        to={"./Login"}
+        className="btn rounded m-5 text-white"
+        style={{ backgroundColor: Salmon }}
+      >
         <i className="fa fa-sign-in"></i> log in
       </Link>
     </div>
