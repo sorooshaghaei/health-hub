c98eb88293cc86955f85965edbf1251162ce5ca8
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-10-26 23:29:26 +0330

    overal visitor works

--- health-hub/src/components/cards/OveralVisitorsCard.jsx
+++ health-hub/src/components/cards/OveralVisitorsCard.jsx
@@ -4,61 +4,93 @@ import barchartIcon from "../../assets/bar_chart.svg";
 import { getAllPatients } from "../../services/patientService";
 
 const OveralVisitorsCard = () => {
-  const [weeklyPatientCounts, setWeeklyPatientCounts] = useState([]);
+  const [currentWeekPatients, setCurrentWeekPatients] = useState(0);
+  const [lastWeekPatients, setLastWeekPatients] = useState(0);
+  const [percentageChange, setPercentageChange] = useState(0);
+
+  // Function to fetch patients for the current week
+  const getAllPatientsCurrentWeek = async () => {
+    const today = new Date();
+    const currentWeekEnd = new Date(today);
+    currentWeekEnd.setDate(today.getDate() - today.getDay() + 6); // End of the current week
+    const currentWeekStart = new Date(today);
+    currentWeekStart.setDate(today.getDate() - today.getDay()); // Start of the current week
+
+    // Filter patients with appointment dates in the current week
+    const response = await getAllPatients(currentWeekStart, currentWeekEnd);
+
+    return {
+      data: response.data.filter((patient) => {
+        const appointmentDate = new Date(patient.appointmentDate);
+        return (
+          appointmentDate >= currentWeekStart &&
+          appointmentDate <= currentWeekEnd
+        );
+      }),
+    };
+  };
+
+  // Function to fetch patients for the week before the current week
+  const getAllPatientsLastWeek = async () => {
+    const today = new Date();
+    const lastWeekEnd = new Date(today);
+    lastWeekEnd.setDate(today.getDate() - today.getDay() - 1); // Go back to the end of the previous week
+    const lastWeekStart = new Date(lastWeekEnd);
+    lastWeekStart.setDate(lastWeekStart.getDate() - 6); // Go back to the start of the previous week
+
+    // Filter patients with appointment dates in the last week
+    const response = await getAllPatients(lastWeekStart, lastWeekEnd);
+
+    return {
+      data: response.data.filter((patient) => {
+        const appointmentDate = new Date(patient.appointmentDate);
+        return (
+          appointmentDate >= lastWeekStart && appointmentDate <= lastWeekEnd
+        );
+      }),
+    };
+  };
 
   const fetchWeeklyPatientCounts = async () => {
     try {
-      const response = await getAllPatients();
-      if (response.data) {
-        const patientsData = response.data;
-        const weeklyCounts = Array(7).fill(0); // Assuming you want data for the last 7 days
-
-        patientsData.forEach((patient) => {
-          if (patient.appointmentDate) {
-            const date = new Date(patient.appointmentDate);
-            const daysAgo = daysBetween(new Date(), date);
-            if (daysAgo >= 0 && daysAgo < 7) {
-              weeklyCounts[daysAgo]++;
-            }
-          }
-        });
-
-        console.log("Patients count by week:", weeklyCounts);
-        setWeeklyPatientCounts(weeklyCounts);
+      // Fetch patients for the current week
+      const currentWeekResponse = await getAllPatientsCurrentWeek();
+      if (currentWeekResponse.data) {
+        setCurrentWeekPatients(currentWeekResponse.data.length);
       } else {
-        console.error("Response data is undefined.");
+        console.error("Response data for the current week is undefined.");
+      }
+
+      // Fetch patients for the week before the current week
+      const lastWeekResponse = await getAllPatientsLastWeek();
+      if (lastWeekResponse.data) {
+        setLastWeekPatients(lastWeekResponse.data.length);
+      } else {
+        console.error(
+          "Response data for the week before the current week is undefined."
+        );
       }
     } catch (error) {
       console.error("Error fetching patients:", error);
     }
   };
 
-  // Function to calculate the number of days between two dates
-  function daysBetween(date1, date2) {
-    const oneDay = 24 * 60 * 60 * 1000;
-    return Math.round(Math.abs((date1 - date2) / oneDay));
-  }
-
   useEffect(() => {
     fetchWeeklyPatientCounts();
-    // eslint-disable-next-line
+    // eslint-disable-next-line 
   }, []);
 
-  const [percentageChange, setPercentageChange] = useState(0);
-
   useEffect(() => {
-    if (weeklyPatientCounts.length >= 2) {
-      const currentWeekPatients =
-        weeklyPatientCounts[weeklyPatientCounts.length - 1];
-      const previousWeekPatients =
-        weeklyPatientCounts[weeklyPatientCounts.length - 2];
+    if (currentWeekPatients >= 2 && lastWeekPatients >= 2) {
+      // Calculate the percentage change
+      const difference = currentWeekPatients - lastWeekPatients;
+      const percentage = ((difference / lastWeekPatients) * 100).toFixed(2);
 
-      const difference = currentWeekPatients - previousWeekPatients;
-      const percentage = ((difference / previousWeekPatients) * 100).toFixed(2);
-
+      console.log("currentweek:", currentWeekPatients);
+      console.log("lastweek:", lastWeekPatients);
       setPercentageChange(percentage);
     }
-  }, [weeklyPatientCounts]);
+  }, [currentWeekPatients, lastWeekPatients]);
 
   return (
     <div>
@@ -71,7 +103,7 @@ const OveralVisitorsCard = () => {
             className="card-title col-md-7 my-3 mx-3"
             style={{ textAlign: "left" }}
           >
-            <b>Overal Visitors A Week</b>
+            <b>Overall Visitors A Week</b>
           </h6>
           <img className="mx-3" src={barchartIcon} alt="barchartIcon" />
         </div>
@@ -79,11 +111,12 @@ const OveralVisitorsCard = () => {
         <div className="row card-body">
           <div className="col">
             <p style={{ textAlign: "left" }}>
-              Increase in data by{" "}
               {percentageChange > 0
-                ? `+${percentageChange}%`
-                : `${percentageChange}%`}{" "}
-              inpatients in the last 7 days
+                ? `Increase of ${percentageChange}%`
+                : percentageChange < 0
+                ? `Decrease of ${Math.abs(percentageChange)}%`
+                : "No change in patient count"}{" "}
+              in the last 7 days
             </p>
           </div>
           <div className="col">
@@ -91,7 +124,9 @@ const OveralVisitorsCard = () => {
               <b style={{ color: LightWhite }}>
                 {percentageChange > 0
                   ? `+${percentageChange}%`
-                  : `${percentageChange}%`}
+                  : percentageChange < 0
+                  ? percentageChange
+                  : "0%"}
               </b>
             </div>
             <div
@@ -101,9 +136,9 @@ const OveralVisitorsCard = () => {
               <span>New</span>
             </div>
           </div>
-        </div>
 
-        <p className="card-text my-3">Last updated 3 mins ago</p>
+          <p className="card-text my-3">Last updated 3 mins ago</p>
+        </div>
       </div>
     </div>
   );
