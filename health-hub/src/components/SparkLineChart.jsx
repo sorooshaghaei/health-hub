9ed995db10598181da9b133ddd6a4b9c6a9ae77c
sorooshaghaei <soroosh77aghaei@gmail.com>
2023-10-26 21:38:33 +0330

    sparkline gets conts of patients

--- health-hub/src/components/SparkLineChart.jsx
+++ health-hub/src/components/SparkLineChart.jsx
@@ -1,16 +1,34 @@
 import React from "react";
-import { Sparklines, SparklinesLine } from "react-sparklines";
-import { Teal } from "../helpers/colors";
+import { Sparklines, SparklinesLine, SparklinesSpots } from "react-sparklines";
+import { Salmon, Teal } from "../helpers/colors";
+import "../styles/SparklineChart.css";
 
+const monthNames = [
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
 
-const SparkLineChart = ({ data }) => {
-  
+const SparkLineChart = ({ data, title }) => {
+  const xValues = monthNames; // Month names for x-axis
+  const yValues = data; // Patient counts for y-axis
+
   return (
-      
-      <div className="mt-3"> 
-      <Sparklines data={data} height={120} width={180} lineColor={Teal}  >
-        <SparklinesLine color={Teal} />
-      </Sparklines>
+    <div className="sparkline-container">
+      {title && <h2>{title}</h2>}
+      <div className="chart-wrapper">
+        <Sparklines data={yValues} height={80} width={120} lineColor={Teal} xvalues={xValues} yvalues={yValues}>
+          <SparklinesLine color={Salmon} />
+          <SparklinesSpots style={{ fill: Salmon }} />
+        </Sparklines>
+      </div>
+      <div className="month-list">
+        {monthNames.map((month, index) => (
+          <div className="month-item" key={index}>
+            <span className="month-name">{month}</span>
+            <span className="patient-count">{data[index]}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- health-hub/src/pages/Dashboard.jsx
+++ health-hub/src/pages/Dashboard.jsx
@@ -7,39 +7,46 @@ import TotalSurgeriesCard from "../components/cards/TotalSurgeriesCard";
 import SparkLineChart from "../components/SparkLineChart";
 
 const Dashboard = ({ patients }) => {
-  const [patientCount, setPatientCount] = useState();
+  const [patientCount, setPatientCount] = useState([]);
 
   const fetchAllPatients = async () => {
     try {
       const response = await getAllPatients();
       if (response.data) {
-        if (response) {
-          const count = response.data.length;
-          console.log("Patients count:", count);
-          return count;
-        } else {
-          console.error("Patients data is undefined.");
-        }
+        const patientsData = response.data;
+        const monthlyCounts = Array(12).fill(0);
+
+        patientsData.forEach((patient) => {
+          if (patient.appointmentDate) {
+            const date = new Date(patient.appointmentDate);
+            const month = date.getMonth();
+            monthlyCounts[month]++;
+          } else {
+            console.warn("Patient data missing 'appointmentDate' property:", patient);
+          }
+        });
+
+        console.log("Patients count by month:", monthlyCounts);
+        return monthlyCounts;
       } else {
         console.error("Response data is undefined.");
       }
-      // Handle these cases gracefully, e.g., return a default value or show an error message.
-      return -1;
+
+      return Array(12).fill(0);
     } catch (error) {
       console.error("Error fetching patients:", error);
-      // Handle the error, e.g., show an error message to the user.
-      return -1;
+      return Array(12).fill(0);
     }
   };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const count = await fetchAllPatients();
-        if (count !== -1) {
-          setPatientCount(count);
+        const monthlyCounts = await fetchAllPatients();
+        if (monthlyCounts.every((count) => count !== -1)) {
+          setPatientCount(monthlyCounts);
         } else {
-          console.log("Failed to retrieve patient count.");
+          console.log("Failed to retrieve patient counts by month.");
           // Handle the case where data retrieval failed.
         }
       } catch (error) {
@@ -51,18 +58,17 @@ const Dashboard = ({ patients }) => {
     fetchData();
   }, []);
 
-  const data = [5, 10, 8, 15, 12, 6, 14, 18];
-
   return (
     <div>
       <div className="">
         <div className="row justify-content-center">
           <div className="col-6 ">
-            <Appointments patients={patients} />
+            {/* You may want to pass the patients data to the Appointments component */}
+            <Appointments patients={patients}  />
           </div>
           <div className="col-4 my-5 py-5 mx-5">
             <div className="card rounded shadow border-0">
-              <SparkLineChart data={data} />
+              <SparkLineChart data={patientCount} />
             </div>
           </div>
         </div>
